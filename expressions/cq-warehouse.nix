e8dda8758946420d84c70b840d16763d73849e25
eyjhb <eyjhbb@gmail.com>
2022-05-08 17:26:25 +0200

    tracking cq-warehouse

---
+++ expressions/cq-warehouse.nix
@@ -0,0 +1,28 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+}:
+
+buildPythonPackage rec {
+  pname = "cq_warehouse";
+  version = "0.6.0";
+  format = "pyproject";
+
+  src = fetchFromGitHub {
+    owner = "gumyr";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-gCkDb2UmyHb+JEnonWkJr+oUkrntpclUMtHN9CBxleQ=";
+  };
+
+  postPatchPhase = ''
+    rm -rf dist
+  '';
+
+  meta = with lib; {
+    description = " A cadquery parametric part collection";
+    homepage = "https://cq-warehouse.readthedocs.io";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ eyjhb ];
+  };
+}
